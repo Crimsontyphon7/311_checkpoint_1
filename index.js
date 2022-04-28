d2558e35fc559c883eb7e84dd8d97bd27ddaf61f
Crimsontyphon7 <lamancio87@gmail.com>
2022-04-27 20:48:45 -0500

    1

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+const usersRouter = require('./routes/users');
 const port = process.env.PORT || 4000
 
+const idCounter = 10;
+global.idCounter = idCounter;
+
+app.use(bodyparser.json());
+app.use('/users', usersRouter);
 app.get('/', (req, res) => res.send('default route'))
 
 app.listen(port, () => {
